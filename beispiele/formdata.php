4b424e8286503e20f70689864d6ce8b836bca1f8
Mellon Smith <mellon385champi@gmail.com>
2022-11-02 08:51:07 +0100

    m2 A8 finished

--- beispiele/formdata.php
+++ beispiele/formdata.php
@@ -11,43 +11,21 @@
 </head>
 <body>
 <?php
-$vornameErr = $nachnameErr = $emailErr = $datenschutzErr = "";
-$vorname = $_POST["vorname"];
-$nachname = $_POST["nachname"];
-$email = $_POST["email"];
-$datenschutz = $_POST["datenschutz"];
-$intervall = $_POST["intervall"];
 
-
-if (empty($_POST["vorname"]) || $_POST["vorname"] == " ") {
-    $vornameErr = "Vorname fehlt.";
-    }
-if (empty($_POST["nachname"]) || $_POST["nachname"] == " ") {
-    $nachnameErr = "Nachname fehlt.";
-}
-if (empty($_POST["email"])) {
-    $emailErr = "E-Mail leer";
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $emailErr = "Ungültiges Format";
-} else if (str_contains($_POST["email"], "rcpt.at") || str_contains($_POST["email"], "damnthespam.at") || str_contains($_POST["email"], "wegwerfmail.de") || str_contains($_POST["email"], "trashmail")) {
-    $emailErr = "Ungültige Adresse";
-}
-if (empty($_POST["datenschutz"])) {
-    $datenschutzErr = "Datenschutzhinweis muss gelesen und akzeptiert werden";
-}
 ?>
 
 <p><?php echo $emailErr;?><br>
     <?php echo $datenschutzErr;?><br>
+    <?php echo $datenschutz;?><br>
     <?php echo $vornameErr;?><br>
    <?php echo $nachnameErr;?>
 <?php if ($vornameErr == "" && $datenschutzErr == "" && $nachnameErr == "" && $emailErr == "") {
     echo "VALIDIERT <br>";
-    $myfile = fopen("formoutput.txt", "w");
-    fwrite($myfile, "vorname,". $vorname);
-    fwrite($myfile, "nachname,". $nachname);
-    fwrite($myfile, "email,". $email);
-    fwrite($myfile, "intervall,". $intervall);
+    $myfile = fopen("formoutput.txt", "a");
+    fwrite($myfile, "vorname,". $vorname.PHP_EOL);
+    fwrite($myfile, "nachname,". $nachname.PHP_EOL);
+    fwrite($myfile, "email,". $email.PHP_EOL);
+    fwrite($myfile, "intervall,". $intervall.PHP_EOL);
     fclose($myfile);
 }
 ?></p>
--- beispiele/newsletteranmeldung.php
+++ beispiele/newsletteranmeldung.php
@@ -1,3 +1,32 @@
+<?php
+$vornameErr = $nachnameErr = $emailErr = $datenschutzErr = "";
+$vorname = $nachname = $email = $datenschutz = $intervall = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $vorname = $_POST["vorname"];
+    $nachname = $_POST["nachname"];
+    $email = $_POST["email"];
+    $datenschutz = $_POST["datenschutz"];
+    $intervall = $_POST["intervall"];
+
+    if (empty($_POST["vorname"]) || $_POST["vorname"] == " ") {
+        $vornameErr = "Vorname fehlt.";
+    }
+    if (empty($_POST["nachname"]) || $_POST["nachname"] == " ") {
+        $nachnameErr = "Nachname fehlt.";
+    }
+    if (empty($_POST["email"])) {
+        $emailErr = "E-Mail leer";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Ungültiges Format";
+    } else if (str_contains($_POST["email"], "rcpt.at") || str_contains($_POST["email"], "damnthespam.at") || str_contains($_POST["email"], "wegwerfmail.de") || str_contains($_POST["email"], "trashmail.")) {
+        $emailErr = "Ungültige Adresse";
+    }
+    if (empty($_POST["datenschutz"])) {
+        $datenschutzErr = "Datenschutzhinweis muss gelesen und akzeptiert werden";
+    }
+}
+?>
 <!DOCTYPE html>
 <!--
 - Praktikum DBWT. Autoren:
@@ -10,30 +39,30 @@
     <title>Newsletter</title>
 </head>
 <body>
-<form action="formdata.php" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <fieldset>
         <legend>Anmeldung</legend>
         <label>Anrede</label>
         <br>
-        <input type="radio" id="FrauID" name="GenderID">
+        <input name="genderf" type="radio" id="FrauID" name="GenderID">
         <label for="FrauID">Frau</label>
         <br>
-        <input type="radio" id="MannID" name="GenderID">
+        <input name="genderm" type="radio" id="MannID" name="GenderID">
         <label for="MannID">Mann</label>
         <br>
         <br>
         <label for="firstnameID"> Vorname*</label><br>
-        <input name="vorname" type="text" size="34" placeholder="Bitte geben Sie Ihren Vornamen ein" id="firstnameID">
+        <input value="<?php echo $vorname ?>" name="vorname" type="text" size="34" placeholder="Bitte geben Sie Ihren Vornamen ein" id="firstnameID">
         <br>
         <label for="lastnameID"> Nachname*</label><br>
-        <input name="nachname" type="text" size="34" placeholder="Bitte geben Sie Ihren Nachnamen ein" id="lastnameID">
+        <input value="<?php echo $nachname ?>" name="nachname" type="text" size="34" placeholder="Bitte geben Sie Ihren Nachnamen ein" id="lastnameID">
         <br>
         <label for="MailID"> Email*</label><br>
-        <input name="email" type="email" size="34" placeholder="Bitte geben Sie Ihre E-Mail ein" id="MailID">
+        <input value="<?php echo $email ?>" name="email" type="text" size="34" placeholder="Bitte geben Sie Ihre E-Mail ein" id="MailID">
         <br>
         <br>
         <label for="MenuID"> Benachrichtigungsintervall*</label><br>
-        <select id="MenuID" name="intervall">
+        <select value="<?php echo $intervall ?>" id="MenuID" name="intervall">
             <option value="Tag">Tag</option>
             <option value="Woche">Woche</option>
             <option value="Monat">Monat</option>
@@ -50,6 +79,20 @@
 
     </fieldset>
 </form>
+<p><?php echo $emailErr;?><br>
+    <?php echo $datenschutzErr;?><br>
+    <?php echo $vornameErr;?><br>
+    <?php echo $nachnameErr;?>
+    <?php if ($_SERVER["REQUEST_METHOD"] == "POST" && $vornameErr == "" && $datenschutzErr == "" && $nachnameErr == "" && $emailErr == "") {
+        echo "Die Anmeldung war erfolgreich. <br>";
+        $myfile = fopen("formoutput.txt", "a");
+        fwrite($myfile, "vorname,". $vorname.PHP_EOL);
+        fwrite($myfile, "nachname,". $nachname.PHP_EOL);
+        fwrite($myfile, "email,". $email.PHP_EOL);
+        fwrite($myfile, "intervall,". $intervall.PHP_EOL);
+        fclose($myfile);
+    }
+    ?></p>
 </body>
 
 </html>
