b21bafb809a69325d7753b6ffca0e76717efffb8
HerrSchltn <luisscholten123@gmail.com>
2022-11-09 13:51:19 +0100

    Haben die ersten zwei Aufgaben gemacht.

---
+++ werbeseite/emensadb.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS emensawerbeseite;
+CREATE DATABASE if not exists emensawerbeseite;
+
+USE emensawerbeseite;
+
+CREATE TABLE if not exists gericht (id int8 primary key , name varchar(80) not null unique , beschreibung varchar(800) not null , erfasst_am date not null, vegetarisch boolean not null default 0, vegan boolean not null default 0 , preis_intern double not null CHECK ( preis_intern > 0 ), preis_extern double not null CHECK (preis_intern < preis_extern));
+
+CREATE TABLE if not exists allergen (code char(4) PRIMARY KEY , name varchar(300) not null , typ varchar(20) not null default 'allergen');
+
+CREATE TABLE if not exists kategorie (id int8 PRIMARY KEY , name varchar(80) not null , eltern_id int8, bildname varchar(200));
+
+CREATE TABLE if not exists gericht_hat_allergen (code char(4), gericht_id int8 not null);
+
+CREATE TABLE IF NOT EXISTS gericht_hat_kategorie (gericht_id int8 not null, kategorie_id int8 not null);
+
