e73086d2ec7f22978d0e19d9e2fe61c2e87e59a8
Mellon Smith <mellon385champi@gmail.com>
2022-11-29 09:05:16 +0100

    Test-commit

--- werbeseite/emensadb.sql
+++ werbeseite/emensadb.sql
@@ -3,35 +3,93 @@ CREATE DATABASE if not exists emensawerbeseite;
 
 USE emensawerbeseite;
 
-CREATE TABLE if not exists gericht (id int8 primary key UNIQUE , name varchar(80) not null unique , beschreibung varchar(800) not null , erfasst_am date not null, vegetarisch boolean not null default 0, vegan boolean not null default 0 , preis_intern double not null CHECK ( preis_intern > 0 ), preis_extern double not null CHECK (preis_intern < preis_extern));
+CREATE TABLE if not exists gericht
+(
+    id           int8 primary key UNIQUE,
+    name         varchar(80)  not null unique,
+    beschreibung varchar(800) not null,
+    erfasst_am   date         not null,
+    vegetarisch  boolean      not null default 0,
+    vegan        boolean      not null default 0,
+    preis_intern double       not null CHECK ( preis_intern > 0 ),
+    preis_extern double       not null CHECK (preis_intern < preis_extern)
+);
 
-CREATE TABLE if not exists allergen (code char(4) PRIMARY KEY UNIQUE, name varchar(300) not null , typ varchar(20) not null default 'allergen');
+CREATE TABLE if not exists allergen
+(
+    code char(4) PRIMARY KEY UNIQUE,
+    name varchar(300) not null,
+    typ  varchar(20)  not null default 'allergen'
+);
 
-CREATE TABLE if not exists kategorie (id int8 PRIMARY KEY UNIQUE , name varchar(80) not null , eltern_id int8 REFERENCES kategorie(id), bildname varchar(200));
+CREATE TABLE if not exists kategorie
+(
+    id        int8 PRIMARY KEY UNIQUE,
+    name      varchar(80) not null,
+    eltern_id int8 REFERENCES kategorie (id),
+    bildname  varchar(200)
+);
 
-CREATE TABLE if not exists gericht_hat_allergen (code char(4) REFERENCES allergen(code), gericht_id int8 not null REFERENCES gericht(id));
+CREATE TABLE if not exists gericht_hat_allergen
+(
+    code       char(4) REFERENCES allergen (code),
+    gericht_id int8 not null REFERENCES gericht (id)
+);
 
-CREATE TABLE IF NOT EXISTS gericht_hat_kategorie (gericht_id int8 not null REFERENCES gericht(id), kategorie_id int8 not null REFERENCES kategorie(id));
+CREATE TABLE IF NOT EXISTS gericht_hat_kategorie
+(
+    gericht_id   int8 not null REFERENCES gericht (id),
+    kategorie_id int8 not null REFERENCES kategorie (id)
+);
 
-CREATE TABLE IF NOT EXISTS newsletteranmeldungen (vorname varchar(300) not null, email varchar(300) unique not null, language varchar(300) );
+CREATE TABLE IF NOT EXISTS newsletteranmeldungen
+(
+    vorname  varchar(300)        not null,
+    email    varchar(300) unique not null,
+    language varchar(300)
+);
 
-CREATE TABLE IF NOT EXISTS besucher (ip varchar(300) not null);
+CREATE TABLE IF NOT EXISTS besucher
+(
+    ip varchar(300) not null
+);
 
 UPDATE
     allergen
-SET
-    name = 'Test-commit'
-Where
-    code = 'a6';
+SET name = 'Test-commit'
+Where code = 'a6';
 
-INSERT INTO gericht VALUE (21, 'Pommes mit Currywurst', 'Stinken nach Jonathan', '12.12.12', 0, 0, 10, 11 );
+INSERT INTO gericht VALUE (21, 'Pommes mit Currywurst', 'Stinken nach Jonathan', '12.12.12', 0, 0, 10, 11);
 
-SELECT * FROM gericht g LEFT JOIN gericht_hat_allergen gha ON g.id = gha.gericht_id RIGHT JOIN allergen a on gha.code = a.code;
+SELECT *
+FROM gericht g
+         LEFT JOIN gericht_hat_allergen gha ON g.id = gha.gericht_id
+         RIGHT JOIN allergen a on gha.code = a.code;
 
-SELECT COUNT(name) AS anzahl, name FROM kategorie k RIGHT JOIN gericht_hat_kategorie ghk on k.id = ghk.kategorie_id GROUP BY name HAVING anzahl > 2 ORDER BY COUNT(name) ASC;
+SELECT COUNT(name) AS anzahl, name
+FROM kategorie k
+         RIGHT JOIN gericht_hat_kategorie ghk on k.id = ghk.kategorie_id
+GROUP BY name
+HAVING anzahl > 2
+ORDER BY COUNT(name) ASC;
 
-SELECT COUNT(code) FROM allergen;
-CREATE TABLE IF NOT EXISTS wunschgericht (id int8 primary key UNIQUE AUTO_INCREMENT, name varchar(300), beschreibung varchar(300), erstellungsdatum date not null, e_id int8 references ersteller(id));
+SELECT COUNT(code)
+FROM allergen;
+CREATE TABLE IF NOT EXISTS wunschgericht
+(
+    id               int8 primary key UNIQUE AUTO_INCREMENT,
+    name             varchar(300),
+    beschreibung     varchar(300),
+    erstellungsdatum date not null,
+    e_id             int UNIQUE,
+   FOREIGN KEY (e_id) references ersteller(id)
+);
 
-CREATE TABLE IF NOT EXISTS ersteller (id int8 primary key UNIQUE AUTO_INCREMENT, email varchar(300) unique not null , name varchar(300) default 'anonym');
+CREATE TABLE IF NOT EXISTS ersteller
+(
+    id    int UNIQUE AUTO_INCREMENT,
+    email varchar(300) unique not null,
+    name  varchar(300) default 'anonym',
+    primary key (id)
+);
 
